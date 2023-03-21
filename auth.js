1e8948505f8c5a818b25b9d5c9246a4ca9ba4a51
Gianluigi Forte <koalakoker@gmail.com>
2023-03-21 07:26:47 +0100

    Implementing authorization

--- auth.js
+++ auth.js
@@ -1,13 +1,15 @@
-const { GCrypto } = require("./lib/gcrypto");
-const { mutableStdout } = require("./lib/mutable");
-const util = require("util");
+import { GCrypto } from "./lib/gcrypto.js";
+import { mutableStdout } from "./lib/mutable.js";
+import { promisify } from "node:util";
+import * as readline from "readline";
+import { httpGet } from "./lib/httpGet.js";
 
-const rl = require("readline").createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: mutableStdout,
   terminal: true,
 });
-const question = util.promisify(rl.question).bind(rl);
+const question = promisify(rl.question).bind(rl);
 
 function inputPass() {
   return new Promise((resolve) => {
@@ -22,11 +24,19 @@ async function login() {
   mutableStdout.muted = false;
   const email = await question("Username(email):");
   const password = await inputPass();
-
   console.log();
-  console.log("username:" + email);
-  console.log("hash pass:" + GCrypto.hash(password));
   rl.close();
+
+  const body = { email: email, password: GCrypto.hash(password) };
+  const url = "https://woven-name-321505.appspot.com/api/auth/";
+  const response = await httpGet(url, body);
+  //const data = await response.text();
+  const token = response.headers.get("x-auth-token");
+  if (!token) {
+    console.log("Not authorized");
+  } else {
+    console.log(token);
+  }
 }
 
 login();
