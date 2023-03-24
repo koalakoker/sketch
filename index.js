d94a672ca16fbacc518c52f26ff81cbaef1f8e56
Gianluigi Forte <koalakoker@gmail.com>
2023-03-24 06:42:26 +0100

    Ckech for private key in envirnoment variable

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import cors from "cors";
 import express from "express";
+import config from "config";
 import { datetime } from "./lib/dateTime.js";
-
 import { router as drawing } from "./routes/drawing.js";
+
+let jwtPrivateKey;
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+jwtPrivateKey = config.get("jwtPrivateKey");
+if (!jwtPrivateKey) {
+  console.log(
+    "Fatal error: glinks_jwtPrivateKey not set in an environment variable"
+  );
+  process.exit(1);
+}
 
 app.use(express.json());
 app.use(cors());
